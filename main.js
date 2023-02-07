5184fa38c1586a1fd28494cf7f1b4fb66f62ea5f
D1abloK1ng <90376062+D1abloK1ng@users.noreply.github.com>
2023-02-06 21:36:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function displayTitle() {
+    document.getElementById("title").innerHTML = "More Information About Me";
+}
+function displayInfo() {
+    document.getElementById("moreInfo").innerHTML = "I am a second year majoring in <br> advancing computer science (ACS) with a <br> dual major in game programming.";
+}
+function displayHobbies() {
+    document.getElementById("hobbies").innerHTML = "I enjoy playing video games, watching anime, <br> playing guitar, reading, and programming. <br> And basically anything nerdy.";
+}
+function displayContactInfo() {
+    document.getElementById("contactInfo").innerHTML = "Here is my contact info. <br> Email: TUCANDER@uat.edu <br> Phone: 480-not-real <br> Address: 1500 fake address st.";
+}
